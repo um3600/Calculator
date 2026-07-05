cd0801bb79539e0611aeb6174ed4287059a7f515
U M A R - B I N - R I A Z <umar.bin.riaz99@gmail.com>
2026-07-05 11:26:41 +0500

    Implement basic arithmetic functions in index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero";
+    }
+    return a / b;
+}
+
+var num1 = prompt("Enter the first number:");
+var num2 = prompt("Enter the second number:");
+
+console.log("Addition:", add(num1, num2));
+console.log("Subtraction:", subtract(num1, num2));
+console.log("Multiplication:", multiply(num1, num2));
+console.log("Division:", divide(num1, num2));
